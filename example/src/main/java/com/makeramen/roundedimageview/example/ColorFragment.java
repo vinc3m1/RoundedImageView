4219510f091b756a1caf12ce7619774e6e3cfbe6
Masayuki Suda <topman31daasuu@gmail.com>
2019-01-09 17:47:47 +0900

    support android X

--- example/src/main/java/com/makeramen/roundedimageview/example/ColorFragment.java
+++ example/src/main/java/com/makeramen/roundedimageview/example/ColorFragment.java
@@ -18,7 +18,7 @@ package com.makeramen.roundedimageview.example;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- example/src/main/java/com/makeramen/roundedimageview/example/ExampleActivity.java
+++ example/src/main/java/com/makeramen/roundedimageview/example/ExampleActivity.java
@@ -17,9 +17,9 @@
 package com.makeramen.roundedimageview.example;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
